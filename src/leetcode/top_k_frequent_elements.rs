9d619ec67f61060f0bc72a76c46bb54f6e2ec4c2
Kosuke Kihara <ashenden1965@gmail.com>
2022-07-22 17:03:51 +0900

    leetcode: Top K Frequent Elements

--- src/leetcode/top_k_frequent_elements.rs
+++ src/leetcode/top_k_frequent_elements.rs
@@ -1,6 +1,29 @@
 struct Solution;
 
-use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap};
+
+#[derive(Eq, PartialEq, Clone, Copy)]
+struct Item {
+    num: i32,
+    count: i32,
+}
+
+impl Ord for Item {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.count.partial_cmp(&self.count).unwrap()
+    }
+}
+
+impl PartialOrd for Item {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match self.count.partial_cmp(&other.count) {
+            Some(core::cmp::Ordering::Equal) => {}
+            ord => return ord,
+        }
+        other.count.partial_cmp(&self.count)
+    }
+}
 
 impl Solution {
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
@@ -14,9 +37,20 @@ impl Solution {
             map_count.insert(num, count);
         }
 
-        let mut counts = map_count.into_iter().collect::<Vec<(i32, i32)>>();
-        counts.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
-        counts.iter().take(k as usize).map(|x| x.0).collect()
+        let mut heap = map_count
+            .into_iter()
+            .map(|(num, count)| Item { num, count })
+            .fold(BinaryHeap::new(), |mut acc, item| {
+                acc.push(item);
+                acc
+            });
+
+        let mut result = Vec::new();
+        for _ in 0..k {
+            result.push(heap.pop().unwrap().num);
+        }
+
+        result
     }
 }
 
